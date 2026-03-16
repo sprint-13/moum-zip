111b306ca91a252f9b28890340b465aab3f78948
Eojin Kim <eojin2820@gmail.com>
2026-03-16 15:31:28 +0900

    [feat] Tab 공통 컴포넌트 구현 (#17)
    * chore: 스토리북 추가 설치 및 global css import 추가
    
    * feat(ui): tab-button 컴포넌트 구현
    
    * feat(ui): tab-button 컴포넌트 스토리 작성
    
    * feat: tabs 컴포넌트 구현
    
    * feat: tabs 컴포넌트 스토리 작성
    
    * chore: tab 관련 컴포넌트 export 추가
    
    * refactor(ui): tabs size prop 중복 제거 및 context 기반 구조로 개선
    
    * chore(ui): tabs 폴더 분리
    
    * chore(ui): 배럴 파일 export 순서 정렬

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,6 @@
 export * from "./ui/button";
 export * from "./ui/gnb";
 export * from "./ui/sheet";
+export * from "./ui/tabs/tab-button";
+export * from "./ui/tabs/tabs";
+export * from "./ui/tabs/underline-tab-button";
