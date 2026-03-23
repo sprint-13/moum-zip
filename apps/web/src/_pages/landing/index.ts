2d219280ad12181464bad3dff5ad3191946c219b
hyebinpark <hoonywhitesocks@gmail.com>
2026-03-23 10:26:18 +0900

    refactor(web): app router 방식으로 페이지 구조 변경

---
+++ apps/web/src/_pages/landing/index.ts
@@ -0,0 +1,4 @@
+export * from "./assets";
+export { CATEGORIES } from "./constants";
+export { CategoryCard } from "./ui/category-card";
+export { IcoChevronDownDouble } from "./ui/ico-chevron-down-double";
