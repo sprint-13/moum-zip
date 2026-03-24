14c0fe2d2de744f0c623ee4b0adc30b1d76e9aa6
byeong <qudcks4885@gmail.com>
2026-03-24 18:21:31 +0900

    fix(web): lint ignore 추가

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
+    // biome-ignore lint: 환경변수는 서버에서만 접근 가능하므로 안전하다.
     url: process.env.DATABASE_URL!,
   },
 });
--- apps/web/src/shared/db/index.ts
+++ apps/web/src/shared/db/index.ts
@@ -2,5 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as scheme from "./scheme";
 
+// biome-ignore lint: 환경변수는 서버에서만 접근 가능하므로 안전하다.
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema: scheme });
