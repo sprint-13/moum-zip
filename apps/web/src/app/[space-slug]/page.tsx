94b59169b10756e8a2fd0e440c17ad65611c3bc7
byeong <qudcks4885@gmail.com>
2026-03-25 10:53:22 +0900

    fix(web): space 레이아웃 조정

--- apps/web/src/app/[space-slug]/page.tsx
+++ apps/web/src/app/[space-slug]/page.tsx
@@ -1,15 +1,15 @@
-import { SpaceBody, SpaceBodyLeft, SpaceBodyRight, SpaceHeader } from "@/_pages/space";
+import { SpaceBody, SpaceBodyLeft, SpaceBodyRight, SpaceHeader } from "@/features/space";
 
 // TODO: 스페이스 별 기능을 동적으로 받아와서 컴포넌트를 생성해야함.
 
 export default function SpacePage() {
   return (
-    <main className="mx-auto w-full max-w-6xl p-6">
-      <SpaceHeader />
+    <>
+      <SpaceHeader title="Dashboard" description="dashboard" />
       <SpaceBody>
         <SpaceBodyLeft></SpaceBodyLeft>
         <SpaceBodyRight></SpaceBodyRight>
       </SpaceBody>
-    </main>
+    </>
   );
 }
