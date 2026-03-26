8bf184663a777757af4fa8d416e2590654e20843
byeong <qudcks4885@gmail.com>
2026-03-26 15:53:43 +0900

    fix(web): `getQueryClient` 함수 분리

---
+++ apps/web/src/shared/lib/get-query-client.ts
@@ -0,0 +1,36 @@
+import { defaultShouldDehydrateQuery, isServer, QueryClient } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        // include pending queries in dehydration
+        shouldDehydrateQuery: (query) => defaultShouldDehydrateQuery(query) || query.state.status === "pending",
+        shouldRedactErrors: (error) => {
+          // We should not catch Next.js server errors
+          // as that's how Next.js detects dynamic pages
+          // so we cannot redact them.
+          // Next.js also automatically redacts errors for us
+          // with better digests.
+          return false;
+        },
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    // 서버에서는 클라이언트마다 항상 새로운 QueryClient를 생성합니다.
+    return makeQueryClient();
+  } else {
+    // 브라우저에서는 QueryClient를 한 번만 생성하여 재사용합니다.
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
