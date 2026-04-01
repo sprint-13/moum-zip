eff26a37109f96443b43b1f60650406bfd8a4430
byeong <qudcks4885@gmail.com>
2026-04-01 17:34:19 +0900

    refactor(web): post-detail 컴포넌트 리팩토링
    - post-detail 내에 있던 컴포넌트 개별 파일로 분리

--- apps/web/src/_pages/bulletin/index.ts
+++ apps/web/src/_pages/bulletin/index.ts
@@ -1,6 +1,9 @@
 export { BulletinInfoCard } from "./ui/bulletin-info-card";
 export { BulletinPopularPostCard } from "./ui/bulletin-popular-post-card";
 export { BulletinTable } from "./ui/bulletin-table";
+export { CommentItem } from "./ui/comment-item";
+export { CommentList } from "./ui/comment-list";
+export { PostArticle } from "./ui/post-article";
 export { PostCategoryGuideCard } from "./ui/post-category-guide-card";
-export { PostDetail } from "./ui/post-detail";
+export { PostInfoCard } from "./ui/post-info-card";
 export { PostWritingTipsCard } from "./ui/post-writing-tips-card";
