33f17716b4b06dfc3a3df8f425a67c653421d824
byeong <qudcks4885@gmail.com>
2026-04-02 11:27:20 +0900

    fix(web): 인자 수정

--- apps/web/src/app/[space-slug]/members/_components/member-right-section.tsx
+++ apps/web/src/app/[space-slug]/members/_components/member-right-section.tsx
@@ -1,6 +1,6 @@
 import { OnlineNowCard, PendingMemberCard, RolesOverviewCard } from "@/_pages/members";
 import { addSpaceMemberAction } from "@/_pages/members/action";
-import { getPendingMembersRemote } from "@/_pages/members/use-cases/get-pending-members";
+import { getPendingMembersUseCase } from "@/_pages/members/use-cases/get-pending-members";
 import { queryAllMembersUseCase } from "@/_pages/members/use-cases/query-all-members";
 import type { SpaceInfo } from "@/entities/spaces";
 import { hasPermission } from "@/features/space";
@@ -14,9 +14,7 @@ interface MemberRightSectionProps {
 export const MemberRightSection = async ({ space, membership }: MemberRightSectionProps) => {
   const [allMembers, { pendingMembers }] = await Promise.all([
     queryAllMembersUseCase(space.spaceId),
-    membership.role === "manager"
-      ? getPendingMembersRemote(Number(space.spaceId))
-      : Promise.resolve({ pendingMembers: [] }),
+    membership.role === "manager" ? getPendingMembersUseCase(space.spaceId) : Promise.resolve({ pendingMembers: [] }),
   ]);
 
   const acceptMember = addSpaceMemberAction.bind(null, space.slug);
