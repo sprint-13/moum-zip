a21cf3b190511182628de71910ed8d7c475b9418
byeong <qudcks4885@gmail.com>
2026-04-02 16:10:04 +0900

    fix(web): 스페이스 캐시 태그 지정
    - 스페이 편집을 위한 캐시 태그 지정

--- apps/web/src/entities/spaces/queries.ts
+++ apps/web/src/entities/spaces/queries.ts
@@ -2,12 +2,14 @@ import { eq, inArray } from "drizzle-orm";
 import { cacheLife, cacheTag } from "next/cache";
 import { db } from "@/shared/db";
 import { type NewSpaceDB, spaces } from "@/shared/db/scheme";
+import { CACHE_TAGS } from "@/shared/lib/cache";
 
 // meetingId -> space 조회 -> location, themeColor, status, modules
 export const spaceQueries = {
   findBySlug: (slug: string) => {
     "use cache";
     cacheLife("max");
+    cacheTag(CACHE_TAGS.space(slug));
     return db.query.spaces.findFirst({
       where: eq(spaces.slug, slug),
     });
