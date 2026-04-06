511e59584fa9250d004337a103dee13842571791
Jeayeong_Hong <hong612644@gmail.com>
2026-04-06 18:38:14 +0900

    refactor(web): mypage ui 엔트리 export 경로 정리

--- apps/web/src/_pages/mypage/index.ts
+++ apps/web/src/_pages/mypage/index.ts
@@ -1,4 +1,4 @@
 export { mypageTabs } from "./mock-data";
 export type { MypageMoimCard } from "./model";
-export { MypageView } from "./ui/mypage-view";
+export { MypageView } from "./ui";
 export { getMypagePageData } from "./use-cases";
