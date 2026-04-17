f3af50fe5711eaecd2948f76cda873e98553a145
Haesol Lee <haesol2022@naver.com>
2026-04-17 10:54:44 +0900

    fix(web): tokenResponse 타입 추가

--- apps/web/src/global.d.ts
+++ apps/web/src/global.d.ts
@@ -1,3 +1,10 @@
+declare namespace google.accounts.oauth2 {
+  interface TokenResponse {
+    error?: string;
+    error_description?: string;
+  }
+}
+
 declare global {
   interface Window {
     google: typeof google;
